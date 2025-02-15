384a63e18f4f44f820fae08aab8a69b73606b82e
추성우 <cntjddn715@gmail.com>
2025-02-15 16:02:25 +0900

    vanilla

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
+import path from "path";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config: any) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
+
+    return config;
+  },
 };
 
-export default nextConfig;
+export default withVanillaExtract(nextConfig);
